b4ebc76a0197054b52b48088a4fd1f3427b14a22
caceyli <cacey@bdnacn.com>
2015-07-07 22:53:02 +0800

    Update UNIXSAPInstallationDynamicScript.java

--- serviceNowP/workspace/serviceNowfpcacey/src/Sybase/UNIXSAPInstallationDynamicScript.java
+++ serviceNowP/workspace/serviceNowfpcacey/src/Sybase/UNIXSAPInstallationDynamicScript.java
@@ -61,7 +61,7 @@ public class UNIXSAPInstallationDynamicScript {
         String cmd_ver1 = "export LIBPATH=$dir/SYS/exe/run\n$dir/SYS/exe/run/saplicense -version";
         String line_ver1 = "";
         // my @lines = &shellcmd($BDNA_Connection_Info{"HostObject"},$cmd_ver1, "sapver");
-		String[] lines_ver1 ={"kernel release 4", "SAP release: 5", "patch number "};
+		String[] lines_ver1 ={"kernel release ", "SAP release: ", "patch number "};
 		for (int i=0; i< lines_ver1.length; i++) {
 			line_ver1 = lines_ver1[i];
 	        Pattern kernelVersionRegex1 = Pattern.compile("kernel release\\s*(\\w+)\\s*$", Pattern.CASE_INSENSITIVE);
@@ -85,7 +85,7 @@ public class UNIXSAPInstallationDynamicScript {
 			String cmd_ver2 = "export LIBPATH=$dir/SYS/exe\n$dir/SYS/exe/run/saplicense -version";
 	        String line_ver2 = "";
 	        // my @lines = &shellcmd($BDNA_Connection_Info{"HostObject"},$cmd_ver1, "sapver");
-			String[] lines_ver2 ={"kernel release 4", "SAP release: 5", "patch number 6 "};
+			String[] lines_ver2 ={"kernel release ", "SAP release: ", "patch number 6 "};
 			for (int i=0; i< lines_ver2.length; i++) {
 				line_ver2 = lines_ver2[i];
 		        Pattern kernelVersionRegex1 = Pattern.compile("kernel release\\s*(\\w+)\\s*$", Pattern.CASE_INSENSITIVE);
@@ -114,7 +114,7 @@ public class UNIXSAPInstallationDynamicScript {
 		String start = "0";
 		String R3transPatches = "";
 		//my @lines = &shellcmd($BDNA_Connection_Info{"HostObject"},$cmd_R3V1, "sapver");
-		String[] lines_R3V1 = {"kernel release 7", "This is R3trans release 8", "R3trans patch information", "9"};
+		String[] lines_R3V1 = {""};
 		for (int i=0; i<lines_R3V1.length; i++) {
 			line_R3V1 = lines_R3V1[i];
 			Pattern kernelVersionRegex1 = Pattern.compile("kernel release\\s*(\\w+)\\s*$", Pattern.CASE_INSENSITIVE);
@@ -133,13 +133,59 @@ public class UNIXSAPInstallationDynamicScript {
 			}else if (R3transPatchesM1.find()) {
 				start="1";
 			}else if (start=="1" && R3transPatchesM2.find()) {
-				R3transPatches += line_R3V1 + "<BDNA>";
+				if (BDNA_Results.get("R3transPatches")==null) {
+					R3transPatches = line_R3V1 + "<BDNA>";					
+				}else {
+					R3transPatches = BDNA_Results.get("R3transPatches") + line_R3V1 + "<BDNA>";
+				}
 				BDNA_Results.put("R3transPatches",R3transPatches);
+			}else if (line_R3V1!= null && line_R3V1.equals("")) {
+				start = "0";	
 			}
 				
 		}
 		
-		
+		if (BDNA_Results.get("R3transPatches")==null) {
+			cmd_R3V1 = "export LIBPATH=$dir/SYS/exe\n$dir/SYS/exe/run/R3trans -V";
+		    line_R3V1 ="";
+			start = "0";
+			R3transPatches = "";
+			//my @lines = &shellcmd($BDNA_Connection_Info{"HostObject"},$cmd_R3V1, "sapver");
+			String[] lines_R3V2 = {"kernel release 7", "This is R3trans release 8", "R3trans patch information", "(9.0.1)", ""};
+			for (int i=0; i<lines_R3V2.length; i++) {
+				line_R3V1 = lines_R3V2[i];
+				Pattern kernelVersionRegex1 = Pattern.compile("kernel release\\s*(\\w+)\\s*$", Pattern.CASE_INSENSITIVE);
+				Pattern kernelVersionRegex2 = Pattern.compile("This is.*R3trans.*release\\s*(\\w+)", Pattern.CASE_INSENSITIVE);
+				Pattern R3transPatchesRegex1 = Pattern.compile("R3trans patch information", Pattern.CASE_INSENSITIVE);
+				Pattern R3transPatchesRegex2 = Pattern.compile("^\\(.*\\).*$", Pattern.CASE_INSENSITIVE);
+				
+				Matcher kernelVersionM1 = kernelVersionRegex1.matcher(line_R3V1);
+				Matcher kernelVersionM2 = kernelVersionRegex2.matcher(line_R3V1);
+				Matcher R3transPatchesM1 = R3transPatchesRegex1.matcher(line_R3V1);
+				Matcher R3transPatchesM2 = R3transPatchesRegex2.matcher(line_R3V1);
+				if (BDNA_Results.get("kernelVersion")==null && kernelVersionM1.find() ) {
+					BDNA_Results.put("kernelVersion",kernelVersionM1.group(1));				
+				}else if (BDNA_Results.get("kernelVersion")==null && kernelVersionM2.find()) {
+					BDNA_Results.put("kernelVersion",kernelVersionM2.group(1));
+				}else if (R3transPatchesM1.find()) {
+					start="1";
+				}else if (start=="1" && R3transPatchesM2.find()) {
+					if (BDNA_Results.get("R3transPatches")==null) {
+						R3transPatches = line_R3V1 + "<BDNA>";					
+					}else {
+						R3transPatches = BDNA_Results.get("R3transPatches") + line_R3V1 + "<BDNA>";
+					}
+					BDNA_Results.put("R3transPatches",R3transPatches);
+				}else if (line_R3V1!= null && line_R3V1.equals("")) {
+					start = "0";	
+				}
+					
+			}
+		}
+
+// my output for all collected info this script to help test this script 	(just for use by myself)	
+		System.out.println("lines_R3V1.length:" + lines_R3V1.length);
+		System.out.println("start:" + start);
 		Iterator<?> iterResult = BDNA_Results.entrySet().iterator(); //test
         while(iterResult.hasNext()){ //test
             Map.Entry entryResult = (Map.Entry) iterResult.next(); //test
@@ -149,10 +195,33 @@ public class UNIXSAPInstallationDynamicScript {
             
         }//test
         
-		
-		
-		
-		
-		
+
+// BDNA output at the end of the script  
+        //if ($BDNA_Results{"licenseServerHost"} || $BDNA_Results{"databaseType"}  ||
+        //    $BDNA_Results{"databaseServerHost"} || $BDNA_Results{"kernelVersion"} ||
+        //    $BDNA_Results{"R3transPatches"} || $BDNA_Results{"TPPatches"}) {
+        //    $BDNA_ResultCode = "com.bdna.cle.scripts.success";
+        //} else {
+        //    $BDNA_ResultCode = "com.bdna.cle.scripts.noData";    	
+        //}
+        //$BDNA_ErrorCode = 0;
+        //$BDNA_MessageBundle = "MessagesBundle";
+        
+    	String BDNA_ResultCode = "";
+        int BDNA_ErrorCode;
+        String BDNA_MessageBundle ="";
+        
+        if (BDNA_Results.get("licenseServerHost")!=null || BDNA_Results.get("databaseType")!=null || BDNA_Results.get("databaseServerHost")!=null || BDNA_Results.get("licenseServerHost")!=null ||BDNA_Results.get("kernelVersion")!=null || BDNA_Results.get("patchNumber")!=null || BDNA_Results.get("R3transPatches")!=null || BDNA_Results.get("databaseServerHost")!=null) {
+            BDNA_ResultCode = "com.bdna.cle.scripts.success";   
+            } else {
+                // $BDNA_ResultCode = "com.bdna.cle.scripts.noData";  
+            	BDNA_ResultCode = "com.bdna.cle.scripts.noData"; 
+            }        
+        //$BDNA_ErrorCode = 0;
+        //$BDNA_MessageBundle = "MessagesBundle";
+        BDNA_ErrorCode = 0;    //test
+        BDNA_MessageBundle = "MessagesBundle";  //test
+         
+
 	}	
 }
